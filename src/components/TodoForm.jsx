90f2af9b00ae0009a379d0756ffcc7f379556053
Pika003 <satyajitmaity0123.com>
2024-05-05 08:21:45 +0530

    local storage bug fix

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,21 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTodo } from '../contexts/TodoContext'
 
 function TodoForm() {
+    const [showcopy, setShowCopy] = useState(false)
+    const [newloc, setNewloc] = useState("")
     const [number, setNumber] = useState("")
     const [address, setAddress] = useState("")
     const [location, setLocation] = useState("KRISHNANAGAR")
+    const [allLocation, setAllLocation] = useState([])
     const {addTodo} = useTodo()
+
+    useEffect(() => {
+      const allLocations = JSON.parse(localStorage.getItem("allLocation"));
+      if (allLocations && allLocations.length > 0) {
+        setAllLocation(allLocations);
+      }
+    }, []);
+
+    useEffect(()=>{
+      localStorage.setItem("allLocation", JSON.stringify(allLocation));
+    },[allLocation])
 
     const add = (e)=>{
       e.preventDefault()
       if(number === "" || address === "") return;
 
-       
       //Edit ........
-      addTodo({number, address: `${address}\n${location}\nSAGAR KRISHNANAGAR, 743373`, completed: false})
+      addTodo({number, address: `${address}\n${location}`, completed: false})
       setNumber("");
       setAddress("");
+    }
+
+    const submitCopy =()=>{
+      setAllLocation(prev => [...prev, newloc]);
+      setShowCopy(false);
+      setNewloc('');
+    }
+
+    const addCopy=()=>{
+      setShowCopy(true);
     }
 
   return (
@@ -34,11 +57,28 @@ function TodoForm() {
             onChange={(e)=> setAddress((e.target.value).toUpperCase())}
             className="resize-none px-4 py-3 rounded-md border-none outline-none w-72 h-12 font-bold text-black bg-gray-600 text-base "
         />
-        <select className='px-4 py-3 rounded-md bg-gray-600 w-72' value={location} onChange={e=>setLocation(e.target.value)}>
-          <option value="KRISHNANAGAR">KRISHNANAGAR</option>
-          <option value="NARAHARIPUR">NARAHARIPUR</option>
-        </select>
-        
+        <div className='flex gap-4 flex-wrap items-center justify-center'>
+          <select className='px-4 py-3 rounded-md bg-gray-600 w-56' value={location} onChange={e=>setLocation(e.target.value)}>
+            {allLocation && (
+              allLocation.map((copy, index)=>(
+                <option key={index} value={copy}>{copy}</option>
+              ))
+            )}
+          </select>
+          <div onClick={addCopy} className='p-3 rounded-md bg-gray-600'>➕</div>
+          {showcopy && (
+            <div className='flex gap-4 flex-wrap items-center justify-center'>
+              <textarea 
+              type="text" 
+              placeholder='Add another copy'
+              value={newloc}
+              onChange={(e)=> setNewloc((e.target.value).toUpperCase())}
+              className="resize-none px-4 py-3 rounded-md border-none outline-none w-56 h-12 font-bold text-black bg-gray-600 text-base "
+              />
+              <div onClick={submitCopy} className='p-3 rounded-md bg-gray-600'>✅</div>
+            </div>
+          )}
+        </div>
         <button 
           className="px-4 py-3 rounded-md w-72 lg:w-32 bg-blue-900 text-white text-base border-none cursor-pointer"
           type='submit'
